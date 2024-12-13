2035c699c6bdcc1c334c6039977ca3532b8e29b5
Bruce Flynn <brucef@ssec.wisc.edu>
2024-12-13 10:20:30 -0600

    remove leap-seconds data

--- rdr-lib/build.rs
+++ rdr-lib/build.rs
@@ -5,7 +5,6 @@ use std::path::{Path, PathBuf};
 
 fn main() -> Result<(), Box<dyn Error>> {
     include_default_configs()?;
-    include_leapsec_list()?;
     Ok(())
 }
 
@@ -20,29 +19,5 @@ fn include_default_configs() -> Result<(), Box<dyn Error>> {
         let dest_path = Path::new(&var_os("OUT_DIR").unwrap()).join(&fname);
         copy(&src_path, dest_path)?;
     }
-    Ok(())
-}
-
-/// Include our default leap-seconds.list file and bail if the file is too old.
-fn include_leapsec_list() -> Result<(), Box<dyn Error>> {
-    // const LEAPSEC_MAX_AGE_SECS: u64 = 86400 * 30;
-    // const LEAPSEC_DOWNLOAD_URL: &str =
-    //     "https://hpiers.obspm.fr/iers/bul/bulc/ntp/leap-seconds.list";
-    //
-    // let src_path = etc_path("leap-seconds.list");
-    // if src_path.metadata()?.modified()?.elapsed()?.as_secs() > LEAPSEC_MAX_AGE_SECS {
-    //     // let msg = format!("{source:?} needs to be updated");
-    //     return Err(format!(
-    //         "{src_path:?} needs to be updated. Download new one from {LEAPSEC_DOWNLOAD_URL}"
-    //     )
-    //     .into());
-    // }
-
-    let src_path = etc_path("leap-seconds.list");
-    // copy the leap-seconds.list from project root to out dir where it it will be
-    // included from in src/time.rs
-    let dest_path = Path::new(&var_os("OUT_DIR").unwrap()).join("leap-seconds.list");
-    copy(src_path, dest_path)?;
-
     Ok(())
 }
